2e93e6aefc8391711fcb0e30363c4d78df8932ef
ArvindGanesan95 <aganes25@uic.edu>
2021-05-03 18:14:29 -0700

    changed file format to json of a variable

--- Utilities/Globals.py
+++ Utilities/Globals.py
@@ -7,12 +7,12 @@ import os
 
 project_path = r'./'
 computations_path = os.path.join(project_path, 'Computations')
-link_structures_path = os.path.join(project_path, 'Computations', r'urlmaps.txt')
+link_structures_path = os.path.join(project_path, 'Computations', r'urlmaps.json')
 url_to_code_map_path = os.path.join(project_path, 'Computations', 'url_code_map.json')
 code_to_url_map_path = os.path.join(project_path, 'Computations', 'code_to_url_map.json')
 url_maps_path = os.path.join(project_path, 'Computations', r'urlmaps.json')
 file_contents_path = os.path.join(project_path, r'url_contents')
 inverted_index_directory_path = os.path.join(project_path, 'Computations', r'inverted_index.p')
 url_page_ranks_path = os.path.join(project_path, 'Computations', r'url_page_ranks.txt')
-stop_words_file_path = os.path.join(os.getcwd(), 'Computations', r'url_page_ranks.txt')
+# stop_words_file_path = os.path.join(os.getcwd(), 'Computations', r'url_page_ranks.txt')
 query_path = os.path.join(project_path, 'QueryEngine', 'queries.txt')
